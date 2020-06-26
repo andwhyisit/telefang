410e9f65ef7c3c7d68f095e0cbd415b26be707ca
andwhyisit <realmmaster26@hotmail.com>
2020-06-27 00:59:39 +1000

    Colour testing tool.

--- components/titlelogo/state_machine.asm
+++ components/titlelogo/state_machine.asm
@@ -1,5 +1,8 @@
 INCLUDE "telefang.inc"
 
+SECTION "Colour Test Sequence Index", WRAM0[$C7CF]
+W_ColourTestSequenceIndex:: ds 2
+
 SECTION "Title Logo State Machine", ROMX[$5300], BANK[$2]
 TitleLogo_GameStateMachine::
     ld a, [W_SystemSubState]
@@ -8,7 +11,7 @@ TitleLogo_GameStateMachine::
     jp hl
     
 TitleLogo_StateTable::
-    dw TitleLogo_StateLoadPalettes,TitleLogo_StateLoadGraphics,TitleLogo_StateSmilesoft,TitleLogo_StateFadeIn,TitleLogo_StateWaittime,TitleLogo_StateFadeOut,TitleLogo_StateBonBon,TitleLogo_StateFadeSetup
+    dw TitleLogo_PseudoStateTestCGBColours,TitleLogo_StateLoadGraphics,TitleLogo_StateSmilesoft,TitleLogo_StateFadeIn,TitleLogo_StateWaittime,TitleLogo_StateFadeOut,TitleLogo_StateBonBon,TitleLogo_StateFadeSetup
     dw TitleLogo_StateFadeIn,TitleLogo_StateWaittime,TitleLogo_StateFadeOut,TitleLogo_StateOpeningCredits,TitleLogo_StateFadeSetup,TitleLogo_StateFadeIn,TitleLogo_StateWaittime,TitleLogo_StateFadeOut
     dw TitleLogo_StateNatsume,TitleLogo_StateFadeSetup,TitleLogo_StateFadeIn,TitleLogo_JumpToTulunk,TitleLogo_StateFadeOut,TitleLogo_StateToNext,TitleLogo_StateFadeSetup,TitleLogo_StateFadeOutToNext
     dw TitleLogo_StateWaittime,TitleLogo_StateFadeOut,TitleLogo_StateTulunk,TitleLogo_StateFadeSetup,TitleLogo_StateFadeIn,TitleLogo_StateWaittime,TitleLogo_StateFadeOut,TitleLogo_StateToNext
@@ -317,3 +320,306 @@ Title_Logo_SGBColourLogic::
     ld d, M_SGB_Pal01 << 3 + 1
     M_AuxJmp Banked_PatchUtils_CommitStagedCGBToSGB_CBE
     ret
+
+SECTION "Test CGB Colours", ROMX[$7000], BANK[$2]
+TitleLogo_PseudoStateTestCGBColours::
+	ld a, [W_GameboyType]
+	cp M_BIOS_CPU_CGB
+	ret nz
+	ld a, [H_JPInput_Changed]
+	and M_JPInput_A
+	ret z
+
+	di
+
+.wfbA
+	ld a, [REG_STAT]
+	and 2
+	jr nz, .wfbA
+
+	ld a, [$9800]
+	ei
+
+	cp $80
+	jp z, .skipInit
+	
+	; #178 in metasprite bank 8 is MetaSprite_zukan_denjuu_name.
+	ld a, $80
+	ld [W_LCDC_MetaspriteAnimationBank], a
+	ld a, 178
+	ld [W_LCDC_MetaspriteAnimationIndex], a
+	ld a, 5
+	ld [W_LCDC_NextMetaspriteSlot], a
+	ld a, $80
+	ld [W_LCDC_MetaspriteAnimationXOffsets + 5], a
+	xor a
+	ld [W_LCDC_MetaspriteAnimationYOffsets + 5], a
+	call LCDC_BeginMetaspriteAnimation
+
+	xor a
+	ld [$DD66], a
+	ld [$DD67], a
+
+	ld a, 1
+	ld [W_CGBPaletteStagedOBP], a
+
+	ld a, $20
+	ld [W_LCDC_MetaspriteAnimationBank], a
+
+	ld de, $8800
+	ld hl, .gfx
+	ld b, $18
+
+.gfxloop
+	di
+
+.wfbB
+	ld a, [REG_STAT]
+	and 2
+	jr nz, .wfbB
+	ld a, [hli]
+	ld [de], a
+	inc e
+	ld a, [hli]
+	ld [de], a
+	ei
+	inc de
+	dec b
+	jr nz, .gfxloop
+
+	ld hl, $9800
+	ld de, $8081
+	ld b, $12
+
+.rowloop
+	ld c, 8
+
+.colloop
+	di
+
+.wfbC
+	ld a, [REG_STAT]
+	and 2
+	jr nz, .wfbC
+
+	ld a, d
+	ld [hli], a
+	ld a, e
+	ld [hli], a
+	ei
+
+	dec c
+	jr nz, .colloop
+	ld c, $82
+	di
+
+.wfbD
+	ld a, [REG_STAT]
+	and 2
+	jr nz, .wfbD
+
+	ld a, c
+	ld [hli], a
+	ld [hli], a
+	ld [hli], a
+	ld [hli], a
+	ei
+
+	ld a, $B
+	add l
+	ld l, a
+	inc hl
+	dec b
+	jr nz, .rowloop
+	ld a, 1
+	ldh [REG_VBK], a
+	ld b, $12
+	ld hl, $9800
+
+.attribrowloop
+	ld de, $800
+
+.attribcolloop
+	di
+
+.wfbE
+	ld a, [REG_STAT]
+	and 2
+	jr nz, .wfbE
+	ld a, e
+	ld [hli], a
+	ld [hli], a
+	ei
+	inc e
+	dec d
+	jr nz, .attribcolloop
+	dec e
+	di
+
+.wfbF
+	ld a, [REG_STAT]
+	and 2
+	jr nz, .wfbF
+
+	ld a, e
+	ld [hli], a
+	ld [hli], a
+	ld [hli], a
+	ld [hli], a
+	ei
+	
+	ld a, $B
+	add l
+	ld l, a
+	inc hl
+	dec b
+	jr nz, .attribrowloop
+
+	xor a
+	ldh [REG_VBK], a
+
+.skipInit
+	ld a, [W_ColourTestSequenceIndex + 1]
+	ld h, a
+	ld a, [W_ColourTestSequenceIndex]
+	ld l, a
+
+	ld de, -1000
+	ld b, 0
+
+.loopthousands
+	add hl, de
+	inc b
+	ld a, h
+	cp $80
+	jr c, .loopthousands
+	ld de, 1000
+	add hl, de
+	dec b
+	ld c, 0
+	call .drawnumbergfx
+
+	ld de, -100
+	ld b, 0
+
+.loophundreds
+	add hl, de
+	inc b
+	ld a, h
+	cp $80
+	jr c, .loophundreds
+
+	dec b
+	ld de, 100
+	add hl, de
+	ld c, $10
+	call .drawnumbergfx
+	ld b, 0
+	ld a, l
+
+.looptens
+	inc b
+	sub 10
+	jr nc, .looptens
+	
+	add 10
+	ld l, a
+	dec b
+	ld c, $20
+	call .drawnumbergfx
+	ld b, l
+	ld c, $30
+	call .drawnumbergfx
+
+	ld a, [W_ColourTestSequenceIndex + 1]
+	ld h, a
+	ld a, [W_ColourTestSequenceIndex]
+	ld l, a
+	add hl, hl
+	add hl, hl
+	add hl, hl
+	add hl, hl
+	add hl, hl
+	ld d, h
+	ld e, l
+
+	ld b, M_LCDC_CGBStagingAreaSize >> 1
+	ld hl, W_LCDC_CGBStagingBGPaletteArea
+
+.colourloop
+	ld a, e
+	ld [hli], a
+	ld a, d
+	ld [hli], a
+	inc de
+	dec b
+	jr nz, .colourloop
+
+	ld a, 1
+	ld [W_CGBPaletteStagedBGP], a
+
+	ld a, [W_ColourTestSequenceIndex + 1]
+	ld h, a
+	ld a, [W_ColourTestSequenceIndex]
+	ld l, a
+	inc hl
+	ld a, h
+	ld [W_ColourTestSequenceIndex + 1], a
+	ld a, l
+	ld [W_ColourTestSequenceIndex], a
+	ret
+
+.drawnumbergfx
+	ld a, b
+	add a
+	add a
+	add a
+	add a
+	push hl
+	ld hl, .numbergfx
+	add l
+	jr nc, .noinch
+	inc h
+
+.noinch
+	ld l, a
+	ld d, $87
+	ld e, c
+	ld b, 8
+
+.numberdrawloop
+	di
+
+.wfbH
+	ld a, [REG_STAT]
+	and 2
+	jr nz, .wfbH
+	ld a, [hli]
+	ld [de], a
+	inc e
+	ld a, [hli]
+	ld [de], a
+	ei
+	inc de
+	dec b
+	jr nz, .numberdrawloop
+
+	pop hl
+	ret
+
+.gfx
+	db $0F, $00, $0F, $00, $0F, $00, $0F, $00, $0F, $00, $0F, $00, $0F, $00, $0F, $00
+	db $0F, $FF, $0F, $FF, $0F, $FF, $0F, $FF, $0F, $FF, $0F, $FF, $0F, $FF, $0F, $FF
+	db $FF, $FF, $FF, $FF, $FF, $FF, $FF, $FF, $FF, $FF, $FF, $FF, $FF, $FF, $FF, $FF
+
+.numbergfx
+	db $00, $00, $18, $18, $24, $24, $24, $24, $24, $24, $24, $24, $18, $18, $00, $00
+	db $00, $00, $08, $08, $08, $08, $08, $08, $08, $08, $08, $08, $08, $08, $00, $00
+	db $00, $00, $38, $38, $04, $04, $04, $04, $18, $18, $20, $20, $3C, $3C, $00, $00
+	db $00, $00, $38, $38, $04, $04, $04, $04, $18, $18, $04, $04, $38, $38, $00, $00
+	db $00, $00, $08, $08, $10, $10, $24, $24, $3C, $3C, $04, $04, $04, $04, $00, $00
+	db $00, $00, $3C, $3C, $20, $20, $20, $20, $38, $38, $04, $04, $38, $38, $00, $00
+	db $00, $00, $1C, $1C, $20, $20, $20, $20, $38, $38, $24, $24, $18, $18, $00, $00
+	db $00, $00, $3C, $3C, $04, $04, $08, $08, $08, $08, $10, $10, $10, $10, $00, $00
+	db $00, $00, $18, $18, $24, $24, $24, $24, $18, $18, $24, $24, $18, $18, $00, $00
+	db $00, $00, $18, $18, $24, $24, $24, $24, $1C, $1C, $04, $04, $18, $18, $00, $00
